200f74df9ecc84ea1f381515afa604b3613dfc09
nicolas2289h <91564340+nicolas2289h@users.noreply.github.com>
2024-04-14 21:33:17 -0300

    Editando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer pt-3 d-flex'>
         <p className='font-italic'><i>Nicolas Huanca -</i></p>
-        <a className='font-italic ms-1' href="https://thin-minute-production.up.railway.app/swagger-ui/index.html" target='_blank'>API Doc</a>
+{/*         <a className='font-italic ms-1' href="https://thin-minute-production.up.railway.app/swagger-ui/index.html" target='_blank'>API Doc</a> */}
     </footer>
   )
 }
